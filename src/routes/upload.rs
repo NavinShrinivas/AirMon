9629324316d245088d3f57802071b198469c2963
NavinShrinivas <karupal2002@gmail.com>
2022-04-04 00:17:04 +0000

    half way upload

--- src/routes/upload.rs
+++ src/routes/upload.rs
@@ -1,4 +1,6 @@
 //The sensors want to upload data
+use std::fs::File;
+
 
 use serde_json::{Result,Value};
 
@@ -6,8 +8,9 @@ pub fn upload_world(){
     println!("Hello world, From upload mod!");
 }
 
-
 pub fn upload_handler(new_data : &str){
    let json_object : Value = serde_json::from_str(&new_data).unwrap();
    println!("upload log : from sensor : {}, new data : {}",json_object["sensor_name"],json_object["new_data"]);
+    let mut write_fd = File::create(format!("{}{}",json_object["sensor_name"].as_str().unwrap(),".txt"));
+    
 }
